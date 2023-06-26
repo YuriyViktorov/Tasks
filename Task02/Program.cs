f3dd02232d712eecb44f09bf9b3f57925477c512
Yuriy Emelin <yuem-1@yandex.ru>
2023-06-26 23:34:16 +0300

    Задание2

---
+++ Task02/Program.cs
@@ -0,0 +1,21 @@
+﻿double firstValue;
+double secondValue;
+
+Console.Write("Введите первое число: ");
+firstValue = double.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+secondValue = double.Parse(Console.ReadLine());
+
+if(firstValue > secondValue)
+{
+    Console.Write(firstValue);
+    Console.Write(">");
+    Console.Write(secondValue);
+}
+else
+{
+    Console.Write(firstValue);
+    Console.Write("<");
+    Console.Write(secondValue);
+}
