71cd92fd5226c9f6e1243f5e6dbd0593079d0ff2
Yuriy Emelin <yuem-1@yandex.ru>
2023-06-27 21:40:48 +0300

    Задание2

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,4 +1,5 @@
-﻿double firstValue;
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+double firstValue;
 double secondValue;
 
 Console.Write("Введите первое число: ");
@@ -10,12 +11,12 @@ secondValue = double.Parse(Console.ReadLine());
 if(firstValue > secondValue)
 {
     Console.Write(firstValue);
-    Console.Write(">");
+    Console.Write(" > ");
     Console.Write(secondValue);
 }
 else
 {
     Console.Write(firstValue);
-    Console.Write("<");
+    Console.Write(" < ");
     Console.Write(secondValue);
 }
